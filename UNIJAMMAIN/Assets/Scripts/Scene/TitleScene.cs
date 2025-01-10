b63667efec7b6f5b9d838554013d63a44fe33d59
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-01-10 15:43:44 +0900

    Feat ; 각종 Managers

---
+++ UNIJAMMAIN/Assets/Scripts/Scene/TitleScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScene : BaseScene
+{
+    public override void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+        
+      
+    }
+    private void Start()
+    {
+        Init();
+
+    }
+
+}
