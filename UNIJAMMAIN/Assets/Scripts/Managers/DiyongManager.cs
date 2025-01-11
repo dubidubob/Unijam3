ebf584bcf366100fc22486a8096a53e9147bf120
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-01-11 21:28:45 +0900

    Feat : Diyong Manager

---
+++ UNIJAMMAIN/Assets/Scripts/Managers/DiyongManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DiyongManager : MonoBehaviour
+{
+    public GameObject W, A, S, D, LeftUp, LeftDown, RightUp, RightDown;
+
+    private GameObject ActionGo;
+    private float upScaleAmount = 1.2f;
+    private void Start()
+    {
+
+        Managers.Input.KeyBoardChecking -= PressButtonArrow;
+        Managers.Input.KeyBoardChecking += PressButtonArrow;
+
+        Managers.Input.KeyArrowcodeAction -= PressButtonKeyBoard;
+        Managers.Input.KeyArrowcodeAction += PressButtonKeyBoard;
+    }
+    void PressButtonArrow(KeyCode attackType) //WASD
+    {
+        string key = attackType.ToString();
+        if (key == "W")
+        {
+            ActionGo = W;
+        }
+        else if (key == "A")
+        {
+            ActionGo = A;
+        }
+        else if (key == "S")
+        {
+            ActionGo = S;
+        }
+        else if (key == "D")
+        {
+            ActionGo = D;
+        }
+        else Debug.Log("어떤것도 행해지지않은 오류");
+
+        ActionGo.transform.DOScale(Vector3.one * upScaleAmount, 0.2f)
+          .OnComplete(() => ActionGo.transform.DOScale(Vector3.one, 0.2f));
+
+    }
+    void PressButtonKeyBoard(GamePlayDefine.RangedAttackType attackType)
+    {
+        string key = attackType.ToString();
+        if (key == "LeftUp")
+        {
+            ActionGo = LeftUp;
+        }
+        else if (key == "LeftDown")
+        {
+            ActionGo = LeftDown;
+        }
+        else if (key == "RightUp")
+        {
+            ActionGo = RightUp;
+        }
+        else if (key == "RightDown")
+        {
+            ActionGo = RightDown;
+        }
+        else Debug.Log("어떤것도 행해지지않은 오류");
+
+        ActionGo.transform.DOScale(Vector3.one * upScaleAmount, 0.2f)
+           .OnComplete(() => ActionGo.transform.DOScale(Vector3.one, 0.2f));
+    }
+
+}
