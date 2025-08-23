909da4c516e3723bba9f0c3dae1c8b0700290c55
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-08-23 17:05:24 +0900

    Fix :GamePlayScene 몹 잘보이게끔 Layer조절

--- UNIJAMMAIN/Assets/MainScene.cs
+++ UNIJAMMAIN/Assets/MainScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
-
+using TMPro;
 public class MainScene : UI_Popup
 {
     public Image brush; // Image 컴포넌트 (Fill 설정된 브러쉬 이미지)
@@ -16,7 +16,18 @@ public class MainScene : UI_Popup
     public Transform leftImage;
     public Transform rightImage;
 
-    public Transform[] buttonsTransform;
+    public RectTransform[] buttonsTransform;
+    public TMP_Text[] tmpText;
+
+    private Material originalMaterial; // 초기값
+    private Vector2[] originalPositions; // 원래 위치 저장용
+
+    enum CanClcikState
+    {
+       CanAnyThing,
+        IsMoving
+    }
+    private CanClcikState currentState = CanClcikState.CanAnyThing;
     enum Buttons
     {
         StoryMode,
@@ -28,6 +39,16 @@ public class MainScene : UI_Popup
 
     private void Start()
     {
+        // 시작할 때 원래 머티리얼 저장
+        originalMaterial = tmpText[0].fontSharedMaterial;
+
+        // 원래 위치 저장
+        originalPositions = new Vector2[buttonsTransform.Length];
+        for (int i = 0; i < buttonsTransform.Length; i++)
+        {
+            originalPositions[i] = buttonsTransform[i].anchoredPosition;
+        }
+
         Init();
         PlayBrushFillAnimation();
     }
@@ -90,19 +111,8 @@ public class MainScene : UI_Popup
 
     private void OptionClicked(PointerEventData eventData)
     {
-        // 설정 진입 로직
-        int index = 1;
+        SetButtonOpen(1);
 
-        //buttonsTransform 0,1 위로
-        for(int i = 0; i<1;i++)
-        {
-            DOTween.To(() => buttonsTransform[i].position.y+300, x => buttonsTransform[i].position = new Vector3(x, 0f, 0f), 0f, comeTime);
-        }
-        // -- 2,3 아래로
-        for (int i = 2; i < 3; i++)
-        {
-            DOTween.To(() => buttonsTransform[i].position.y - 300, x => buttonsTransform[i].position = new Vector3(x, 0f, 0f), 0f, comeTime);
-        }
         // 가운데 이미지 페이드인
     }
 
@@ -115,5 +125,59 @@ public class MainScene : UI_Popup
     {
         // 게임 종료 로직
     }
+
+    private void SetButtonOpen(int index)
+    {
+        //buttonsTransform 위로
+        for (int i = 0; i < index+1; i++)
+        {
+            buttonsTransform[i].DOAnchorPosY(buttonsTransform[i].anchoredPosition.y + 300, 1f) // 1f = 이동 시간
+               .SetEase(Ease.OutCubic);
+        }
+
+        //buttonsTransform 아래로
+        for (int i = index+1; i < 4; i++)
+        {
+            buttonsTransform[i].DOAnchorPosY(buttonsTransform[i].anchoredPosition.y - 300, 1f) // 1f = 이동 시간
+               .SetEase(Ease.OutCubic);
+        }
+
+        //buttonsTransform 하이라이트
+        TextGlow(index);
+
+        
+    }
+
+    private void TextGlow(int index)
+    {
+        tmpText[index].fontMaterial = new Material(tmpText[index].fontSharedMaterial);
+
+        // Glow (Underlay)
+        tmpText[index].fontMaterial.EnableKeyword("UNDERLAY_ON");
+        tmpText[index].fontMaterial.SetColor("_UnderlayColor", new Color(1f, 1f, 0f, 0.8f)); // 노란빛
+        tmpText[index].fontMaterial.SetFloat("_UnderlaySoftness", 0.8f);
+        tmpText[index].fontMaterial.SetFloat("_UnderlayDilate", 0.5f);
+
+        // Outline
+        tmpText[index].fontMaterial.SetFloat("_OutlineWidth", 0.2f);
+        tmpText[index].fontMaterial.SetColor("_OutlineColor", Color.yellow);
+    }
+
+    private void ResetHighlight(int index)
+    {
+        // 원래 머티리얼로 복원
+        tmpText[index].fontMaterial = originalMaterial;
+    }
+
+    public void ResetButtons()
+    {
+        // 원래 자리로 되돌리기
+        for (int i = 0; i < buttonsTransform.Length; i++)
+        {
+            buttonsTransform[i].DOAnchorPos(originalPositions[i], 1f)
+                .SetEase(Ease.OutCubic);
+        }
+    }
+
 }
 
