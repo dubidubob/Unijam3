bf8a5065c58cf7171b77858dcb79fed96c72e6ab
Soyeon Joo <sildredel@naver.com>
2025-08-24 18:00:46 +0900

    WIP Rank

--- UNIJAMMAIN/Assets/Scripts/Utils/IngameData.cs
+++ UNIJAMMAIN/Assets/Scripts/Utils/IngameData.cs
@@ -16,4 +16,18 @@ public static class IngameData
             }
         }
     }
+
+    public static int TotalMobCnt { set; get; }
+    public static int PerfectMobCnt { set; get; }
+    public static int GoodMobCnt { set; get; }
+    public static int WrongInputCnt { set; get; }
+    public static int AttackedMobCnt { set; get; }
+    public static void RankInit()
+    {
+        TotalMobCnt = 0;
+        PerfectMobCnt = 0;
+        GoodMobCnt = 0;
+        WrongInputCnt = 0;
+        AttackedMobCnt = 0;
+    }
 }
