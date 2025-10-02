2b268047814388931f4db52b2d2b09ae6b72e765
YejunKim02 <102803368+YejunKim02@users.noreply.github.com>
2025-10-02 22:23:21 +0900

    bgm 최신화 / 스토리씬, 게임씬

--- UNIJAMMAIN/Assets/Scripts/Scene/MainTitle.cs
+++ UNIJAMMAIN/Assets/Scripts/Scene/MainTitle.cs
@@ -16,7 +16,7 @@ public class MainTitle : BaseScene
     {
         Init();
         Managers.UI.ShowPopUpUI<MainScene>();
-        Managers.Sound.Play("BGM/MainScene_V1", Define.Sound.BGM);
+        Managers.Sound.Play("BGM/MainScene_V2", Define.Sound.BGM);
 
     }
 
