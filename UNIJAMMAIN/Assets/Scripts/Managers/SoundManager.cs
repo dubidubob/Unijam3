8e53009e3beb46ddcdcf0ca237577af82c7c4426
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-10-03 17:08:51 +0900

    Fix : Setting Init될때도

--- UNIJAMMAIN/Assets/Scripts/Managers/SoundManager.cs
+++ UNIJAMMAIN/Assets/Scripts/Managers/SoundManager.cs
@@ -41,6 +41,7 @@ public class SoundManager
 
             SFX = _audioSources[(int)Define.Sound.SFX];
         }
+        SettingNewSceneVolume();
     }
     public void Play(AudioClip audioClip, Define.Sound type = Define.Sound.SFX, float pitch = 1.0f,float volume = 1.0f)
     {
