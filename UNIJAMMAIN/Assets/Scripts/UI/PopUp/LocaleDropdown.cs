f128b0404a574c8e745abc01876f9fdb7cffca00
Soyeon Joo <sildredel@naver.com>
2026-02-21 05:58:20 +0900

    feat: Drop down마다 언어 바뀜
    TODO: 디자인 다듬기

---
+++ UNIJAMMAIN/Assets/Scripts/UI/PopUp/LocaleDropdown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Localization.Settings;
+
+public class LocaleDropdown : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown dropdown;
+
+    private IEnumerator Start()
+    {
+        yield return LocalizationSettings.InitializationOperation;
+
+        var availableLocales = LocalizationSettings.AvailableLocales.Locales;
+
+        dropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentLocaleIndex = 0;
+
+        for (int i = 0; i < availableLocales.Count; i++)
+        {
+            var locale = availableLocales[i];
+
+            options.Add(locale.LocaleName);
+
+            if (LocalizationSettings.SelectedLocale == locale)
+            {
+                currentLocaleIndex = i;
+            }
+        }
+
+        dropdown.AddOptions(options);
+
+        dropdown.value = currentLocaleIndex;
+
+        dropdown.onValueChanged.AddListener(LocaleSelected);
+    }
+
+    // 드롭다운 값이 변경될 때마다 호출되는 함수
+    private void LocaleSelected(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+}
