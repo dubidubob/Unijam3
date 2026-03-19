e3fbc3a13e995ca3ff2d9d9240d2942c5afc2ad9
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2026-03-19 16:30:31 +0900

    Fix : 프롤로그컨트롤러 위치수정

--- UNIJAMMAIN/Assets/Scripts/Prologue/PrologueController.cs
+++ UNIJAMMAIN/Assets/Scripts/Prologue/PrologueController.cs
@@ -113,6 +113,19 @@ public class PrologueController : MonoBehaviour
             SceneLoadingManager.Instance.NotifySceneReady();
         }
 
+
+        // 1. 현재 오디오 엔진 시간에서 0.5초(혹은 1초) 뒤를 시작 시간으로 설정합니다.
+        // 이 짧은 대기 시간 동안 오디오 파일 로딩 및 버퍼링이 안전하게 끝납니다.
+        double musicStartTime = AudioSettings.dspTime + 0.5f;
+
+        // 2. 사운드 매니저를 통해 "musicStartTime에 정확히 재생하라"고 예약합니다.
+        // (PhaseController에 쓰신 Managers.Sound.PlayScheduled를 그대로 사용)
+        Managers.Sound.PlayScheduled("BGM/Prolog", musicStartTime, Define.Sound.BGM);
+
+        // 3. 프레임이 아닌, 절대적인 오디오 시간이 시작 시간이 될 때까지 대기합니다.
+
+        await UniTask.WaitUntil(() => AudioSettings.dspTime >= musicStartTime);
+
         PlayPrologueSequence().Forget();
         PlayTextSequence().Forget();
     }
@@ -224,17 +237,7 @@ public class PrologueController : MonoBehaviour
     // ==========================================
     private async UniTaskVoid PlayPrologueSequence()
     {
-        // 1. 현재 오디오 엔진 시간에서 0.5초(혹은 1초) 뒤를 시작 시간으로 설정합니다.
-        // 이 짧은 대기 시간 동안 오디오 파일 로딩 및 버퍼링이 안전하게 끝납니다.
-        double musicStartTime = AudioSettings.dspTime + 0.5f;
-
-        // 2. 사운드 매니저를 통해 "musicStartTime에 정확히 재생하라"고 예약합니다.
-        // (PhaseController에 쓰신 Managers.Sound.PlayScheduled를 그대로 사용)
-        Managers.Sound.PlayScheduled("BGM/Prolog", musicStartTime, Define.Sound.BGM);
-
-        // 3. 프레임이 아닌, 절대적인 오디오 시간이 시작 시간이 될 때까지 대기합니다.
-
-        await UniTask.WaitUntil(() => AudioSettings.dspTime >= musicStartTime);
+    
         foreach (var action in prologueSequence)
         {
             List<UniTask> tasks = new List<UniTask>();
